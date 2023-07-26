38845ae2d542fe82833e056a5b898dc829d5ebaa
“JuanSeGA” <117958247+JuanSeGArtor@users.noreply.github.com>
2023-07-26 17:53:42 -0500

    remover paquete breeze con sus dependencias y recursos

---
+++ resources/views/partials/navAdmin.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">SSF</a>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-auto gap-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Sondeos</a>
+                </li>
+            </ul>
+        </div>
+        <div class="text-light">
+            Perfil
+        </div>
+    </div>
+</nav>
