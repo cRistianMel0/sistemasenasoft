867be59406a329bc8edbadfb58384d88c7a939ab
CristianMM <118846884+cRistianMel0@users.noreply.github.com>
2023-07-27 13:25:53 -0500

    reparar error de sintaxis en el modelo sondeo
    fataba un = despues deñ $fillable

--- app/Models/Sondeo.php
+++ app/Models/Sondeo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sondeo extends Model
 {
-    protected $fillable[
+    protected $fillable=[
         'idSondeo',
         'idAdministrador',
         'idTema',
