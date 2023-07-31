b9fa5c3b2ea1997b144e40d0b5cc6f808bd31350
CristianMM <118846884+cRistianMel0@users.noreply.github.com>
2023-07-31 13:11:47 -0500

    agregar ruta index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Sondeo;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $sondeos = Sondeo::all();
+        
+    return view('index', ['sondeos'=>$sondeos]);
 });
 
 
 
+Route::get('index', 'SondeosController@index')->name('index');
